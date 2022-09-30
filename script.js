21b669ab6bc8733555302ca8b92579f93db4cbda
Zdravko Gyurov <zdravko.gyurov97@gmail.com>
2022-09-30 12:20:35 +0300

    make header responsive

--- script.js
+++ script.js
@@ -2,28 +2,43 @@ const THEME_KEY = "theme";
 const THEME_DARK_VALUE = "dark";
 const THEME_LIGHT_VALUE = "light";
 
-const toggleLightModeBtn = document.getElementById("enable-light-mode-btn");
-const toggleDarkModeBtn = document.getElementById("enable-dark-mode-btn");
-
-toggleLightModeBtn.addEventListener("click", function () {
-  enableDarkMode();
+const lightModeBtns = document.getElementsByClassName("enable-light-mode-btn");
+Array.from(lightModeBtns).forEach((lightModeBtn) => {
+  lightModeBtn.addEventListener("click", function () {
+    enableDarkMode();
+  });
 });
 
-toggleDarkModeBtn.addEventListener("click", function () {
-  enableLightMode();
+const darkModeBtns = document.getElementsByClassName("enable-dark-mode-btn");
+Array.from(darkModeBtns).forEach((darkModeBtn) => {
+  darkModeBtn.addEventListener("click", function () {
+    enableLightMode();
+  });
 });
 
 function enableDarkMode() {
   document.body.classList.add("lightMode");
-  toggleLightModeBtn.style.display = "none";
-  toggleDarkModeBtn.style.display = "inline";
+
+  Array.from(lightModeBtns).forEach((lightModeBtn) => {
+    lightModeBtn.style.display = "none";
+  });
+  Array.from(darkModeBtns).forEach((darkModeBtn) => {
+    darkModeBtn.style.display = "inline";
+  });
+
   localStorage.setItem(THEME_KEY, THEME_DARK_VALUE);
 }
 
 function enableLightMode() {
   document.body.classList.remove("lightMode");
-  toggleDarkModeBtn.style.display = "none";
-  toggleLightModeBtn.style.display = "inline";
+
+  Array.from(lightModeBtns).forEach((lightModeBtn) => {
+    lightModeBtn.style.display = "inline";
+  });
+  Array.from(darkModeBtns).forEach((darkModeBtn) => {
+    darkModeBtn.style.display = "none";
+  });
+
   localStorage.setItem(THEME_KEY, THEME_LIGHT_VALUE);
 }
 
